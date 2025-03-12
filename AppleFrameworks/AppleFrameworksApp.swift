1e7f5d054b5472396d61781272eb97d080a79deb
deryosssss <w2115152@westminster.ac.uk>
2025-03-12 12:27:17 +0000

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Derya Baglan on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
