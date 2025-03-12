1f51df99c4448cd8e13aa08fd1e501e8cc67951b
deryosssss <w2115152@westminster.ac.uk>
2025-03-12 13:51:55 +0000

    initial framework grid view

---
+++ AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Derya Baglan on 12/03/2025.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
